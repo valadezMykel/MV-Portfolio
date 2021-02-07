984364b720372798da596792c76802170c9732f3
Mykel Valadez <valadezmykel@gmail.com>
2021-02-07 13:34:24 -0800

    fixed styling on profile page

--- src/components/homeComponents/ProfileImage.js
+++ src/components/homeComponents/ProfileImage.js
@@ -1,57 +1,94 @@
 import React from 'react'
 import { Row, Col, Image, Container } from 'react-bootstrap'
+import { GetScreenState } from '../../contexts/screenSizeContext'
 
 export default function ProfileImage(props) {
     
-    const image = {
-        objectFit: "cover",
-        width: '30vw',
-        height: '80vh',
-        objectPosition: '0 0',
-        position: 'relative',
-        zIndex: '4',
-        float: props.reversed ? 'right' : 'left',
-        border: '5px solid black',
-        marginRight: props.reversed ? '0px' : '50px',
-        marginLeft: props.reversed ? '50px' : '0px'
-    }
+    const isMobile = GetScreenState()
 
     const stylings = {
-        maxWidth: '800px',
-        minHeight: '55vh',
-        backgroundColor: props.reversed ? 'rgb(230,225,215)' : 'white',
-        // 'rgb(214,242,255, .7)', 
-        // 'rgb(230,225,215)',
-        padding: '5%',
-        marginTop: '5%',
-        margin: '10%',
-        border: '1vw solid rgb(214,212,113)',
-        fontSize: '20px',
-        position: 'relative',
-        zIndex: '3',
+
+        textDiv: {
+            maxWidth: '800px',
+            minHeight: '55vh',
+            backgroundColor: props.reversed ? 'rgb(230,225,215)' : 'white',
+            // 'rgb(214,242,255, .7)', 
+            // 'rgb(230,225,215)',
+            padding: '5%',
+            marginTop: '5%',
+            margin: '10%',
+            border: '1vw solid rgb(214,212,113)',
+            fontSize: '20px',
+            position: 'relative',
+            left: isMobile ? '' : '5vw',
+            zIndex: '3',
+
+        },
+
+        image: {
+            objectFit: "cover",
+            width: '30vw',
+            height: '80vh',
+            objectPosition: '40% 40%',
+            position: 'relative',
+            zIndex: '4',
+            float: props.reversed ? 'right' : 'left',
+            border: '5px solid black',
+            // marginRight: props.reversed ? '0px' : '5vw',
+            // marginLeft: props.reversed ? '50px' : '0px'
+        },
+
+
 
         h3: {
             color: 'rgb(149,180,185)'
         }
     }
 
+    let profilePiece;
+
+    if(isMobile) {
+        profilePiece = (
+            <Row>
+                <Col sm>
+                    <Image src={props.image} style={stylings.image} />
+                </Col>
+                <Col sm>
+                    <div style={stylings.textDiv}>
+                        <h3 style={stylings.h3}>
+                            {props.title}
+                        </h3>
+                        <br/>
+                        <p style={{}}>
+                            {props.text} 
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+        )
+    } else {
+        profilePiece = (
+
+            <Row>
+                <Col md >
+                    <Image src={props.image} style={stylings.image} />
+                    <div style={stylings.textDiv}>
+                        <h3 style={stylings.h3}>
+                            {props.title}
+                        </h3>
+                        <br/>
+                        <p style={{}}>
+                            {props.text} 
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+        )
+    }
     return (
         <div style={{backgroundColor: props.backgroundColor, paddingTop: '10px', paddingBottom: '25px'}}>
         <Container className='mt-5'>
-        <Row>
-            <Col>
-                <Image src={props.image} style={image} />
-                <div style={stylings}>
-                    <h3 >
-                        {props.title}
-                    </h3>
-                    <br/>
-                    <p style={{}}>
-                        {props.text} 
-                    </p>
-                </div>
-            </Col>
-        </Row>
+            {profilePiece}
         </Container>
         </div>
     )
